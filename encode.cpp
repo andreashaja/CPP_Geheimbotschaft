dcf7ece5366578705ccb28763ca275161fa6da2b
Andreas Haja <ah@andreashaja.com>
2020-10-20 12:07:21 +0000

    updated main program

--- encode.cpp
+++ encode.cpp
@@ -29,34 +29,37 @@ int main()
 
     if (input == "v")
     {
-        cout << "Bitte Text zum VERschlüsseln eingeben: ";
-        encode = true;
+      cout << "Bitte Text zum VERschlüsseln eingeben: ";
+      encode = true; 
     }
-    else if (input == "e")
+    else if(input == "e")
     {
-        cout << "Bitte Text zum ENTschlüsseln eingeben: ";
+      cout << "Bitte Text zum ENTschlüsseln eingeben: ";
     }
     else
     {
-        cout << "Ungültige Eingabe!" << endl;
-        exit(EXIT_FAILURE);
+      cout << "Ungültige Eingabe!" << endl;
+      exit(EXIT_FAILURE);
     }
-    string text; 
-    getline(cin, text);
-
+      
     ///////////////
     // Teil 2 : Berechnung des verschlüsselten Texts
     // (Hinweis: Die Entschlüsselung ist Teil einer Übungsaufgabe)
     ///////////////
+
+    string text; 
+    getline(cin, text);
+
     int index = 0;
     string code = "";
-    while (index < text.size())
+    while(index < text.size())
     {
-        int ascii = int(text[index]);
-        int ascii_code = ascii + (index % 2) + 3;
-        code = code + char(ascii_code);
-
-        index = index + 1;
+      int ascii = int(text[index]);
+      int ascii_code = ascii + (index % 2) + 3;
+      code = code + char(ascii_code);  
+      
+      index = index + 1;
     }
     cout << "Codierter Text = " << code << endl;
+    
 }
